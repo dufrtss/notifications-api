d4b39efe73727dcc522541bbd3cfffc61921c453
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2024-11-24 12:49:40 -0300

    cleaning the environment :broom:

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{js,mjs,cjs,ts}']
+  },
+  {
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  {
+    rules: {
+      'quotes': [2, 'single', { 'avoidEscape': true }]
+    }
+  }
+];
